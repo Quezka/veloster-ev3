4158299f3bb4398280a8916c5435ab8ffd520bd1
Quezka <arsdom15@outlook.com>
2024-01-22 13:15:02 +0100

    Started writing the logic for checking the distance to the wall.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pybricks.parameters import Port, Stop, Direction, Color, Button
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-
+import time
 ev3 = EV3Brick()
 msx = Motor(Port.A)
 msd = Motor(Port.D)
@@ -13,4 +13,20 @@ radar = InfraredSensor(Port.S4)
 
 robot = DriveBase(msx, msd, 43, 152)
 
+startUpFase = False
+runningFase = False
+def checkDistanceToWall(InfraredSensor):
+    distance = InfraredSensor.distance()
+    if distance == 20 or distance < 20:
+        return distance, "Distanza < o = 20%"
+
+
+while True:
+    time.sleep(1)
+    checkDistanceToWall(radar)
+
+while startUpFase:
+    proceed = False
+
 robot.straight(100)
+
