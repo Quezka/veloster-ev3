5036bde3f1cb44edf22203058524f5730719560a
Quezka <arsdom15@outlook.com>
2024-02-09 11:12:46 +0100

    Upgraded the logic a bit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pybricks.parameters import Port, Stop, Direction, Color, Button
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-import time
+
 ev3 = EV3Brick()
 msx = Motor(Port.A)
 msd = Motor(Port.D)
@@ -18,12 +18,15 @@ runningFase = False
 def checkDistanceToWall(InfraredSensor):
     distance = InfraredSensor.distance()
     if distance == 20 or distance < 20:
-        return distance, "Distanza < o = 20%"
+        return "Distanza <= 20%"
+    else:
+        return "Distanza > 20%"
 
 
 while True:
-    time.sleep(1)
-    checkDistanceToWall(radar)
+    ev3.wait(100)
+    ev3.screen.clear()
+    ev3.scrren.print(checkDistanceToWall(radar))
 
 while startUpFase:
     proceed = False
